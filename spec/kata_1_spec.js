8f09ebd1b69d830ebad061c4f4b633bcbded9ea2
ruslankotliar <ruslankot101@gmail.com>
2024-05-26 17:28:13 +0200

    Cleanup

--- spec/kata_1_spec.js
+++ spec/kata_1_spec.js
@@ -1,60 +1,6 @@
-describe('Self Study 1 – TDD Coding Kata “Help ALF“', function() {
+describe('Self Study 2 – TDD Pair Programming Coding Kata “Electrons around the cores“', function() {
     beforeEach(function() {
-        space = new Space();
+        example = new Example();
     });
-
-    // Todo List:
-    // 1. Should return the correct coordinates of the spaceship when it's found in the center of the map.
-	it('should return the correct coordinates when the spaceship is in the center of the map', function() {
-		const map = "......\n" +
-				  "......\n" +
-				  "..X...\n" +
-				  "......\n" +
-				  "......\n"
-		expect(space.findSpaceship(map)).toEqual([2, 2]);
-	});
-
-    // 2. Should return the correct coordinates of the spaceship when it's found at the top-left corner of the map.
-	it('should return the correct coordinates when the spaceship is at the top-left corner of the map', function() {
-		const map = "X......\n" +
-				  "......\n" +
-				  "......\n" +
-				  "......\n" +
-				  "......\n"
-		expect(space.findSpaceship(map)).toEqual([0, 0]);
-	});
-
-    // 3. Should return the correct coordinates of the spaceship when it's found at the bottom-right corner of the map.
-	it('should return the correct coordinates when the spaceship is at the bottom-right corner of the map', function() {
-		const map = "......\n" +
-				  "......\n" +
-				  "......\n" +
-				  "......\n" +
-				  "......X\n"
-		expect(space.findSpaceship(map)).toEqual([4, 6]);
-	});
-
-    // 4. Should return "Spaceship lost forever." if the spaceship is not found in the map.
-	it('should return "Spaceship lost forever." if the spaceship is not found in the map', function() {
-		const map = "......\n" +
-				  "......\n" +
-				  "......\n" +
-				  "......\n" +
-				  "......\n"
-		expect(space.findSpaceship(map)).toEqual("Spaceship lost forever.");
-	});
-
-
-    // 5. Should handle a map with irregular shapes and sizes.
-	it('should handle a map with irregular shapes and sizes', function() {
-		const map = "......\n" +
-				  "......\n" +
-				  "......\n" +
-				  "......\n" +
-				  "......X\n" +
-				  "......\n" +
-				  "......\n"
-		expect(space.findSpaceship(map)).toEqual([4, 6]);
-	});
 });
 
--- src/kata_1.js
+++ src/kata_1.js
@@ -1,17 +1,5 @@
-function Space() {}
+function Example() {}
 
-const ERROR_MSG = "Spaceship lost forever.";
-const SPACESHIP = 'X';
-const NEW_LINE = '\n';
+Example.prototype.run = function() {
 
-Space.prototype.findSpaceship = function(map) {
-  const lines = map.split(NEW_LINE);
-  const rowIndex = lines.findIndex(line => line.includes(SPACESHIP));
-  
-  if (rowIndex !== -1) {
-      const columnIndex = lines[rowIndex].indexOf(SPACESHIP);
-      return [rowIndex, columnIndex];
-  } else {
-      return ERROR_MSG;
-  }
 };
