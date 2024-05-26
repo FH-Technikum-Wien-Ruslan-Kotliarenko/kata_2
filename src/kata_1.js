1ab6d9700679b88722e6f4431c8460aa3a2922fd
ruslankotliar <ruslankot101@gmail.com>
2024-05-26 17:52:36 +0200

    Step 2: simplest production code – Ruslan

--- src/kata_1.js
+++ src/kata_1.js
@@ -1,5 +1,15 @@
 function Game() {}
 
-Game.prototype.play = function() {
-    
+Game.prototype.play = function(dice) {
+    let totalPetals = 0;
+        
+    dice.forEach(die => {
+        if (die === 3) {
+            totalPetals += 2;
+        } else if (die === 5) {
+            totalPetals += 4;
+        }
+    });
+
+    return totalPetals;
 };
