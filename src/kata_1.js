63d32466e22c8399c15bd322baa244b1e675af14
ruslankotliar <ruslankot101@gmail.com>
2024-05-26 17:59:20 +0200

    Write comments to describe the program – Ruslan

--- src/kata_1.js
+++ src/kata_1.js
@@ -1,12 +1,23 @@
 class Game {
+    /**
+     * Constructs a new Game object.
+     * Initializes the PETALS_MAP property to map die numbers to their corresponding number of petals.
+     */
     constructor() {
+        // PETALS_MAP maps die numbers to their corresponding number of petals
         this.PETALS_MAP = {
-            3: 2,
-            5: 4
+            3: 2, // Die number 3 has 2 petals
+            5: 4  // Die number 5 has 4 petals
         };
     }
 
+    /**
+     * Calculates the total number of petals around the cores based on the rolled dice.
+     * @param {number[]} dice - The array of rolled dice numbers.
+     * @returns {number} The total number of petals around the cores.
+     */
     play(dice) {
+        // Use reduce method to calculate the total number of petals
         return dice.reduce((totalPetals, die) => totalPetals + (this.PETALS_MAP[die] || 0), 0);
     }
 }
