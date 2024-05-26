588789ad36ac1543966da66ad8f1c26b4315289e
ruslankotliar <ruslankot101@gmail.com>
2024-05-26 18:05:59 +0200

    Create 18 test cases in total – Lukas & Ruslan

--- spec/kata_1_spec.js
+++ spec/kata_1_spec.js
@@ -30,5 +30,102 @@ describe('Self Study 2 – TDD Pair Programming Coding Kata “Electrons around 
         const result = game.play(input);
         expect(result).toBe(expectedOutput);
     });
-});
 
+    it('should return 8 for the input [4, 2, 5, 5, 4]', function() {
+        const input = [4, 2, 5, 5, 4];
+        const expectedOutput = 8;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 8 for the input [2, 5, 1, 3, 3]', function() {
+        const input = [2, 5, 1, 3, 3];
+        const expectedOutput = 8;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 8 for the input [2, 5, 2, 5, 6]', function() {
+        const input = [2, 5, 2, 5, 6];
+        const expectedOutput = 8;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 6 for the input [6, 2, 5, 3, 2]', function() {
+        const input = [6, 2, 5, 3, 2];
+        const expectedOutput = 6;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 0 for the input [4, 2, 1, 1, 2]', function() {
+        const input = [4, 2, 1, 1, 2];
+        const expectedOutput = 0;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 6 for the input [5, 6, 3, 6, 6]', function() {
+        const input = [5, 6, 3, 6, 6];
+        const expectedOutput = 6;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 0 for the input [2, 6, 2, 2, 4]', function() {
+        const input = [2, 6, 2, 2, 4];
+        const expectedOutput = 0;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 2 for the input [3, 2, 4, 2, 1]', function() {
+        const input = [3, 2, 4, 2, 1];
+        const expectedOutput = 2;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 2 for the input [4, 4, 2, 6, 3]', function() {
+        const input = [4, 4, 2, 6, 3];
+        const expectedOutput = 2;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 8 for the input [2, 1, 5, 1, 5]', function() {
+        const input = [2, 1, 5, 1, 5];
+        const expectedOutput = 8;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 8 for the input [1, 5, 5, 2, 4]', function() {
+        const input = [1, 5, 5, 2, 4];
+        const expectedOutput = 8;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 6 for the input [5, 2, 4, 6, 3]', function() {
+        const input = [5, 2, 4, 6, 3];
+        const expectedOutput = 6;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 6 for the input [1, 6, 1, 5, 3]', function() {
+        const input = [1, 6, 1, 5, 3];
+        const expectedOutput = 6;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });
+    
+    it('should return 2 for the input [1, 4, 3, 2, 6]', function() {
+        const input = [1, 4, 3, 2, 6];
+        const expectedOutput = 2;
+        const result = game.play(input);
+        expect(result).toBe(expectedOutput);
+    });    
+});
